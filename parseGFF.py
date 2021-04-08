273e849734c6d22ddc2d9465ac76ed0a60946a2c
clstacy <clstacy@uark.edu>
2021-04-07 23:02:01 -0500

    Correct mistake in reverse complementation clause of parseGFF.py script

--- parseGFF.py
+++ parseGFF.py
@@ -37,7 +37,7 @@ with open(args.gff, 'r') as gff_in:
     # print header line for each fasta sequence:
     print(">" + genome.id, header_info)#, ";", "STRAND", strand)
     # determine whether to reverse complement
-    if header_info == "-": # if -, need to reverse and complement the strand
+    if strand == "-": # if -, need to reverse and complement the strand
       # print the reverse complement of nucleotides in the sequence 
       print(genome.seq[start-1:end].reverse_complement())
       #note that the -1 is due to 0 indexing of python vs 1 indexing of gff format. not on 'end' b/c of normal py indexing rules excludes last value
